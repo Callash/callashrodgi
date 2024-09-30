c2595d1b34b1c4b50a305ceb29235857559275a9
Callash <CALLASHRODGERS@GMAIL.COM>
2024-09-30 20:09:58 +0300

    Create Exercise 2.py

---
+++ Exercise 2.py
@@ -0,0 +1,11 @@
+def is_palindrome(s: str) -> bool:
+    normalized_str = ''.join(s.split()).lower()
+    return normalized_str == normalized_str[::-1]
+
+def filter_even_numbers(numbers: list) -> list:
+    
+    return [num for num in numbers if num % 2 == 0]
+
+print(is_palindrome("A man a plan a canal Panama"))  # Output: True
+
+print(filter_even_numbers([1, 2, 3, 4, 5, 6]))  # Output: [2, 4, 6]
